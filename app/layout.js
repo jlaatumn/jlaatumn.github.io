e424c49ea55197bc8abd30efbf91d59ebbc43fc7
philip_nguyen <iphilipn@gmail.com>
2024-11-07 00:39:39 -0600

    website template, basically just moved what ever was in the html file

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,40 @@
 import { Provider } from "@/components/ui/provider"
 
+import { Box, Container } from '@chakra-ui/react'
+import Navbar from '../components/navbar';
+
+
 export default function RootLayout({ children }) {
   return (
     <html suppressHydrationWarning>
       <body>
-        <Provider>{children}</Provider>
+        <Provider>
+          <Navbar />
+          {children}
+        </Provider>
+        
       </body>
     </html>
   );
 }
+
+
+
+
+
+
+
+// import { ChakraProvider } from '@chakra-ui/react';
+// // import Navbar from '../components/navbar.js';
+
+// export default function Layout({ children }) {
+//   return (
+//     <ChakraProvider>
+//       <Provider>{children}</Provider>
+//     </ChakraProvider>
+//   );
+// }
+
+
+
+
--- app/page.js
+++ app/page.js
@@ -1,25 +1,37 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Button } from "@/components/ui/button"
-import { HStack } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  HStack
+} from "@chakra-ui/react";
 
-// const Demo = () => {
-//   return (
-//     <HStack>
-//       <Button>Click me</Button>
-//       <Button>Click me</Button>
-//     </HStack>
-//   )
-// }
+
 
 export default function Home() {
   return (
-    <>
-    <h1>hello world</h1>
-    <HStack>
-       <Button>Click me</Button>
-       <Button>Click me</Button>
-     </HStack>
-    </>
+    <Container bg="#939f93" maxW="100%" p={0} h="100vh">
+      
+      <Container p={5} color="black">
+        <Box 
+        borderRadius="lg" 
+        bg="#edebea"
+        p={3} 
+        mb={6} 
+        align="center">
+            Hello, welcome to the JLA website. This is a box
+        </Box>
+      </Container>
+
+      <Container p={5} mb={6} align="center">
+        <HStack justify="center" align="center">
+          <Button>button1</Button>
+          <Button>button2</Button>
+        </HStack>
+      </Container>
+      
+    
+    </Container>
   );
 }
